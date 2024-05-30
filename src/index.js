a5ab2cda9c979438000dbf6419a1ecacce59c27e
D'Amore Vincenzo <vincenzo.damore@luxottica.com>
2024-05-30 07:14:09 +0200

    first import

---
+++ src/index.js
@@ -0,0 +1,42 @@
+import 'dotenv/config'
+import express from 'express'
+import puppeteer from 'puppeteer'
+
+const app = express()
+
+app.get('/', async (req, res) => {
+
+  const browser = await puppeteer.launch()
+  console.log('browser created!')
+
+  const page = await browser.newPage()
+
+  await page.goto(`chrome://version`)
+
+  page.on('console', msg => console.log('PAGE LOG:', msg.text()))
+
+  await page.evaluate(() => {
+      window.resizeTo(100, 100)
+      console.log("window resized!!")
+  });
+  
+  let opt = {
+    type: 'png',
+    fullPage: true,
+    encoding: 'binary',
+    captureBeyondViewport: true
+  }
+
+  let resp = await page.screenshot(opt)
+
+  await page.close()
+  await browser.close()
+
+  res.type('png') // => 'image/png'
+  res.send(resp)
+})
+
+app.listen(process.env.PORT, async () => {
+  console.log(`Example app listening on port ${process.env.PORT}`)
+  // server ready to accept connections here
+})
